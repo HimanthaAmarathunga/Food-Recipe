ce3027ddd939f117aa7beed956b5d291ad875d42
Senadeera S.A.P.R <82633809+Savvy9899@users.noreply.github.com>
2022-04-10 10:56:13 +0530

    splash screen updated

--- food_recipe_final/lib/pages/SplashScreen/splash_screen.dart
+++ food_recipe_final/lib/pages/SplashScreen/splash_screen.dart
@@ -27,12 +27,12 @@ class _SplashScreenState extends State<SplashScreen>
   }
 
   late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 2),
+    duration: const Duration(seconds: 4),
     vsync: this,
   );
 
   late final Animation<Offset> _leftToRightAnim = Tween<Offset>(
-    begin: const Offset(-1.5, 0.0),
+    begin: const Offset(0.0, 0.0),
     end: Offset.zero,
   ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
 
@@ -50,28 +50,38 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
+            // Container(
+            //   width: 200,
+            //   height: 200,
+            //   decoration: BoxDecoration(
+            //       color: primary,
+            //       borderRadius: BorderRadius.circular(20),
+            //       image: const DecorationImage(
+            //           image.asset(
+            //               "https://i.pinimg.com/originals/1b/34/30/1b34300f23f4a8a00020af4d661497d4.gif"),
+            //           fit: BoxFit.cover)),
+            // ),
+            Image.asset(
+              'assets/images/waiter.gif',
               width: 200,
               height: 200,
-              decoration: BoxDecoration(
-                  color: primary,
-                  borderRadius: BorderRadius.circular(20),
-                  image: const DecorationImage(
-                      image: NetworkImage(
-                          "https://i.pinimg.com/originals/1b/34/30/1b34300f23f4a8a00020af4d661497d4.gif"),
-                      fit: BoxFit.cover)),
+              fit: BoxFit.cover,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SlideTransition(
                   position: _leftToRightAnim,
-                  child: const Text(''),
+                  child: const Text(
+                    "Digital Chef",
+                    textScaleFactor: 2,
+                  ),
                 ),
                 SlideTransition(
                   position: _rightToLeftAnim,
